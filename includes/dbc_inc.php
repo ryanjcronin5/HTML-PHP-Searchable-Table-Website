7e65341ea9f56a67c59c1627b6ecb0182a4cabaa
Ryan Cronin <81655542+ryanjcronin5@users.noreply.github.com>
2023-10-17 15:37:19 +1300

    Update dbc_inc.php
    Documentation

--- includes/dbc_inc.php
+++ includes/dbc_inc.php
@@ -1,6 +1,10 @@
 <?php
+// Create a new MySQLi database connection
 $conn = new mysqli('localhost', 'root', '', 'rcronin_main');
+
+// Check if the connection was successful
 if($conn->connect_error){
+	// If there's an error connecting to the database, terminate the script and display an error message
 	die("Error Connecting to Database: " . $conn->connect_error);
 }
 ?>
