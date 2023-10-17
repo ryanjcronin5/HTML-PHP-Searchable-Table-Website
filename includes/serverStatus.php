129b477adb3c474580a989d215079acea8ea5e8b
Ryan Cronin <81655542+ryanjcronin5@users.noreply.github.com>
2023-10-17 15:38:02 +1300

    Update serverStatus.php
    Documentation

--- includes/serverStatus.php
+++ includes/serverStatus.php
@@ -1,6 +1,9 @@
 <?php
-  include 'includes/dbc_inc.php';
-  $statusVariables = [
+// Include the database connection script ('dbc_inc.php')
+include 'includes/dbc_inc.php';
+
+// Define an array of specific MySQL server status variables to retrieve
+$statusVariables = [
     'Connections',
     'Uptime',
     'Threads_connected',
@@ -16,13 +19,23 @@
     'Aborted_connects',
     'Max_used_connections'
 ];
+
+// Loop through each status variable and retrieve its value from the MySQL server
 foreach ($statusVariables as $variable) {
-  $sql = "SHOW GLOBAL STATUS LIKE '$variable'";
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    echo "<b>{$variable}:</b> " . $row['Value'] . "<br>";
-  }
+    // Define an SQL query to retrieve the value of the current status variable
+    $sql = "SHOW GLOBAL STATUS LIKE '$variable'";
+
+    // Execute the SQL query and store the result
+    $result = $conn->query($sql);
+
+    // Check if there are rows in the result
+    if ($result->num_rows > 0) {
+        // Fetch the row (there should be only one) and display the variable name and its value
+        $row = $result->fetch_assoc();
+        echo "<b>{$variable}:</b> " . $row['Value'] . "<br>";
+    }
 }
+
+// Close the database connection
 $conn->close();
 ?>
